c5393965d2e8c56c778b10e13192bc9e869709a3
K. Yurov <kirillisokay@icloud.com>
2023-12-05 20:27:26 +0200

    booking types, events

--- components/cal/AppBookingEvent.vue
+++ components/cal/AppBookingEvent.vue
@@ -10,13 +10,41 @@
                         @click="isModalOpen.toggleBooking()" />
                 </div>
             </template>
-            <slot />
+          <UForm :state="state" @submit="saveEvent">
+            <div>
+              <div>{{ state.start }} - {{ state.end }}</div>
+            </div>
+            <UFormGroup label="Name" name="name">
+              <UInput v-model="state.content" label="Name" variant="outline" color="primary" type="text"/>
+            </UFormGroup>
+            <UFormGroup label="Phone" name="phone">
+              <UInput v-model="state.title" variant="outline" color="primary" type="phone" />
+            </UFormGroup>
+            <UButton type="submit" :loading="state.loading" label="Save" class="mt-4" >
+              Submit
+            </UButton>
+          </UForm>
         </UCard>
     </UModal>
 </template>
 
 <script setup lang="ts">
+import type { FormSubmitEvent } from "#ui/types";
+import { useBookingEventStore } from "~/composables/bookingEventStore";
 const isModalOpen = useBookingStore()
+
+const state = reactive({
+  start: undefined,
+  end: undefined,
+  title: undefined,
+  content: undefined,
+  loading: false,
+})
+
+const saveEvent = async (event: FormSubmitEvent<any>) => {
+  useBookingEventStore().saveEvent(event.data)
+  console.log(event.data)
+}
 
 </script>
 
--- utils/supabaseUtils.ts
+++ utils/supabaseUtils.ts
@@ -1,24 +1,24 @@
-import { UploadAvatarParams } from "~/interface";
+import type { UploadAvatarParams } from "~/interface";
 
 export async function uploadAvatar({files, emit}: UploadAvatarParams):Promise<void> {
     const supabase = useSupabaseClient();
     const uploading = ref(false);
     try {
       uploading.value = true;
-  
+
       if (!files || files.length === 0) {
         throw new Error('You must select an image to upload.');
       }
-  
+
       const file = files[0];
       const fileExt = file.name.split('.').pop();
       const fileName = `${Math.random()}.${fileExt}`;
       const filePath = `${fileName}`;
-  
+
       let { error: uploadError } = await supabase.storage.from('avatars').upload(filePath, file);
-  
+
       if (uploadError) throw uploadError;
-  
+
       emit('update:path', filePath);
       emit('upload');
     } catch (error:any) {
@@ -28,7 +28,7 @@ export async function uploadAvatar({files, emit}: UploadAvatarParams):Promise<vo
     }
   }
 
-export async function downloadImage(imgSrc: {value:string}, path:{ value:string} ) { 
+export async function downloadImage(imgSrc: {value:string}, path:{ value:string} ) {
     const client = useSupabaseClient()
     try {
         const { data, error } = await client.storage.from('avatars').download(path.value)
